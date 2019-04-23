3efed55adbde2cf513b3f1b986aa0250e80379a3
Brooklyn Photos <guoneng.list@gmail.com>
2019-04-23 23:47:21 +0200

    demoed and compared the two approaches

--- src/photos/brooklyn/threads/executors/CounterRace.java
+++ src/photos/brooklyn/threads/executors/CounterRace.java
@@ -1,25 +1,31 @@
 package photos.brooklyn.threads.executors;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CounterRace {
-    int counter;
-    final Object lock = new Object();
     public static void main(String[] args) {
-        final CounterRace cr = new CounterRace();
+        run(new CounterWithLock());
+        run(new CounterWithAtomic());
+    }
+
+    static void run(Counter cr){
+        final LocalDateTime start = LocalDateTime.now();
         final List<Future<?>> tasks = new ArrayList<>(8);
         final ExecutorService ec = Executors.newFixedThreadPool(8);
         try {
             for (int i = 0; i < 4; i++) {
-                tasks.add(ec.submit(cr.new Incrementer()));
+                tasks.add(ec.submit(cr.getIncrementer()));
             }
             for (int i = 0; i < 4; i++) {
-                tasks.add(ec.submit(cr.new Decrementer()));
+                tasks.add(ec.submit(cr.getDecrementer()));
             }
             for (Future<?> t : tasks) {
                 try {
@@ -28,10 +34,31 @@ public class CounterRace {
                     e.printStackTrace();
                 }
             }
-            System.out.println(cr.counter);
+            System.out.println(cr.getCounter());
+            System.out.println(ChronoUnit.MICROS.between(LocalDateTime.now(), start));
         }finally {
             ec.shutdown();
         }
+    }
+}
+
+class CounterWithLock implements Counter {
+    private int counter;
+    final Object lock = new Object();
+
+    @Override
+    public Runnable getIncrementer() {
+        return new Incrementer();
+    }
+
+    @Override
+    public int getCounter() {
+        return counter;
+    }
+
+    @Override
+    public Runnable getDecrementer() {
+        return new Decrementer();
     }
 
     class Incrementer implements Runnable {
@@ -57,4 +84,48 @@ public class CounterRace {
             }
         }
     }
+}
+
+class CounterWithAtomic implements Counter {
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    @Override
+    public Runnable getIncrementer() {
+        return new Incrementer();
+    }
+
+    @Override
+    public int getCounter() {
+        return counter.get();
+    }
+
+    @Override
+    public Runnable getDecrementer() {
+        return new Decrementer();
+    }
+
+    class Incrementer implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 1000; i++) {
+                counter.incrementAndGet();
+            }
+        }
+    }
+
+    class Decrementer implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 1000; i++) {
+                counter.decrementAndGet();
+            }
+        }
+    }
+}
+interface Counter {
+    Runnable getIncrementer();
+    int getCounter();
+    Runnable getDecrementer();
 }
